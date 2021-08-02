fcf520611a3eebeb2d5f4fa602bccb1aa4299998
wangzhizhou <wangzhizhou@bytedance.com>
2021-08-02 21:39:07 +0800

    添加qq群发送消息逻辑

--- src/main/java/com/jokerhub/paper/plugin/orzmc/events/PlayerEvents.java
+++ src/main/java/com/jokerhub/paper/plugin/orzmc/events/PlayerEvents.java
@@ -7,23 +7,38 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.net.URL;
+
 public class PlayerEvents implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         System.out.println(player.displayName() + "Join!!");
+
+        String msg = player.displayName() + "上线了！";
+        sendQQGroupMsg(msg);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         System.out.println(player.displayName() + "Quit!!");
+
+        String msg = player.displayName() + "下线了！";
+        sendQQGroupMsg(msg);
     }
 
     @EventHandler
     public void onPlayerKickLeave(PlayerKickEvent event) {
         Player player = event.getPlayer();
         System.out.println(player.displayName() + "KickLeave!!");
+
+        String msg = player.displayName() + "被踢了！";
+        sendQQGroupMsg(msg);
+    }
+
+    void sendQQGroupMsg(String msg) {
+        // TODO: 调用OPQ消息API发送消息到QQ群
     }
 }
