a04b752ef1dd1bad48a7063bcb014e0112d3370c
wangzhizhou <wangzhizhou@bytedance.com>
2021-08-11 21:25:34 +0800

    解决废弃API调用

--- src/main/java/com/jokerhub/paper/plugin/orzmc/commands/GuideBook.java
+++ src/main/java/com/jokerhub/paper/plugin/orzmc/commands/GuideBook.java
@@ -1,11 +1,12 @@
 package com.jokerhub.paper.plugin.orzmc.commands;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -34,28 +35,41 @@ public class GuideBook implements CommandExecutor {
         bookMeta.setTitle("新手指南");
         bookMeta.setAuthor("腐竹");
 
-        BaseComponent[] page = new ComponentBuilder("欢迎新朋友来到我的世界！")
-                .append("\n\n")
-                .append("服务器中一些热爱创造的小伙伴在这里花费了大量心力建造出了各种漂亮的建筑，希望刚加入的朋友不要随意对其进行破坏，尊重他人的劳动成果。做一个有素质的MC玩家!")
-                .append("\n\n")
-                .append("相关链接").color(ChatColor.BOLD)
-                .append("\n")
-                .append("1. ").append("服务器主页").color(ChatColor.BLUE).underlined(true)
-                .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.jokerhub.cn"))
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("点击前往主页").create()))
-                .append("\n").reset()
-                .append("2. ").append("玩家手册").color(ChatColor.BLUE).underlined(true)
-                .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.jokerhub.cn/user/"))
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("点击查看玩家手册").create()))
-                .append("\n").reset()
-                .append("3. ").append("加入玩家QQ群").color(ChatColor.BLUE).underlined(true)
-                .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://jq.qq.com/?_wv=1027&k=DUEQuLE6"))
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("点击加入QQ群").create()))
-                .create();
-        bookMeta.spigot().addPage(page);
+        Style linkStyle = Style.style()
+                .color(TextColor.fromCSSHexString("#5555FF"))
+                .decorate(TextDecoration.UNDERLINED)
+                .build();
+        TextComponent page2 = Component.text()
+                .append(Component.text("欢迎新朋友来到我的世界！"))
+                .append(Component.newline())
+                .append(Component.newline())
+                .append(Component.text("服务器中一些热爱创造的小伙伴在这里花费了大量心力建造出了各种漂亮的建筑，希望刚加入的朋友不要随意对其进行破坏，尊重他人的劳动成果。做一个有素质的MC玩家!"))
+                .append(Component.newline())
+                .append(Component.newline())
+                .append(Component.text("相关链接"))
+                .append(Component.newline())
+                .append(Component.text("1. "))
+                .append(Component.text("服务器主页",linkStyle)
+                        .clickEvent(ClickEvent.openUrl("https://minecraft.jokerhub.cn"))
+                        .hoverEvent(HoverEvent.showText(Component.text("点击前往主页")))
+                )
+                .append(Component.newline())
+                .append(Component.text("2. "))
+                .append(Component.text("玩家手册",linkStyle)
+                        .clickEvent(ClickEvent.openUrl("https://minecraft.jokerhub.cn/user/"))
+                        .hoverEvent(HoverEvent.showText(Component.text("点击前往主页")))
+                )
+                .append(Component.newline())
+                .append(Component.text("3. "))
+                .append(Component.text("加入玩家QQ群",linkStyle)
+                        .clickEvent(ClickEvent.openUrl("https://jq.qq.com/?_wv=1027&k=DUEQuLE6"))
+                        .hoverEvent(HoverEvent.showText(Component.text("点击前往主页")))
+                )
+                .build();
+        bookMeta.addPages(page2);
 
         guideBook.setItemMeta(bookMeta);
         player.openBook(guideBook);
-        player.sendMessage("你获得了" + ((TextComponent) bookMeta.title()).content());
+        player.sendMessage("你获得了" + bookMeta.getTitle());
     }
 }
