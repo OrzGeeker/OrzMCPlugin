f8ab7ef0b5caed86938a51b9214c1b364c00e952
wangzhizhou <wangzhizhou@bytedance.com>
2021-08-18 02:21:54 +0800

    TNT点燃监控添加QQ群通知

--- src/main/java/com/jokerhub/paper/plugin/orzmc/events/TNTEvent.java
+++ src/main/java/com/jokerhub/paper/plugin/orzmc/events/TNTEvent.java
@@ -6,20 +6,25 @@ import com.jokerhub.paper.plugin.orzmc.qqbot.QQBotEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class TNTEvent implements Listener {
     @EventHandler
     public void onTNTPrime(TNTPrimeEvent event) throws Exception {
-
-        Block block = event.getBlock();
-        int x = block.getX();
-        int y = block.getY();
-        int z = block.getZ();
-
         event.setCancelled(true);
+
+        Entity entity = event.getPrimerEntity();
+        if(entity == null) { return; }
+
+        Location loc = entity.getLocation();
+        int x = loc.getBlockX();
+        int y = loc.getBlockY();
+        int z = loc.getBlockZ();
+
         TextComponent msg = Component.text("坐标: ")
                 .append(Component.text( x + " " + y + " " + z).color(TextColor.fromCSSHexString("#00FF00")))
                 .append(Component.space())
