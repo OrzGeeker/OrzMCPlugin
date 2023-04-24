16d9918bc1c7a784bafa436f828819284dafc5e7
wangzhizhou <wangzhizhou@bytedance.com>
2023-04-24 20:11:58 +0800

    add english into the whitelist tip message

--- src/main/java/com/jokerhub/paper/plugin/orzmc/events/WhiteListEvent.java
+++ src/main/java/com/jokerhub/paper/plugin/orzmc/events/WhiteListEvent.java
@@ -6,7 +6,6 @@ import com.jokerhub.paper.plugin.orzmc.OrzMC;
 import com.jokerhub.paper.plugin.orzmc.qqbot.QQBotEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -21,8 +20,12 @@ public class WhiteListEvent implements Listener {
             return;
         }
         String qqGroupId = OrzMC.config().getString("qq_group_id");
+        String discordChannelLink = OrzMC.config().getString("discord_link");
         TextComponent kickMsg = Component.text(player.getName())
-                .append(Component.text(" 不在白名单中，请先加入QQ群: " + qqGroupId + " 联系管理员进行添加"));
+                .append(Component.text(" 不在白名单中，请先加入QQ群: " + qqGroupId + " 联系管理员进行添加"))
+                .append(Component.newline())
+                .append(Component.text("you are not in the whitelist, you can join the discord channel: " + discordChannelLink + " and ask the admin add your id into the whitelist!")
+                );
         event.kickMessage(kickMsg);
 
         // 通知QQ群
