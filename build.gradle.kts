2469f1ddb88ad1d52e8455ba4f21e1905e86d838
wangzhizhou <wangzhizhou@bytedance.com>
2024-09-18 11:28:26 +0800

    adjust gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
-
 plugins {
     kotlin("jvm") version "2.0.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -9,11 +7,10 @@ group = "com.jokerhub.paper.plugin"
 version = "1.0"
 description = "OrzMC"
 
+// Paper Project Setup: https://docs.papermc.io/paper/dev/project-setup
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "papermc-repo"
-    }
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://oss.sonatype.org/content/groups/public/") {
         name = "sonatype"
     }
@@ -21,17 +18,16 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    api("org.apache.httpcomponents:httpasyncclient:4.1.5")
-    implementation("net.dv8tion:JDA:5.0.0-beta.8") {
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    implementation("org.apache.httpcomponents:httpasyncclient:4.1.5")
+    implementation("net.dv8tion:JDA:5.1.0") {
         exclude(module="opus-java")
     }
+    implementation("org.java-websocket:Java-WebSocket:1.5.7")
 }
 
-val targetJavaVersion = 22
-kotlin {
-    jvmToolchain(targetJavaVersion)
-
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(22))
 }
 
 tasks.build {
@@ -40,15 +36,6 @@ tasks.build {
 
 tasks.withType<JavaCompile> {
     options.compilerArgs.add("-Xlint:deprecation")
-}
-
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("paper-plugin.yml") {
-        expand(props)
-    }
 }
 
 tasks.jar {
--- src/main/java/com/jokerhub/paper/plugin/orzmc/commands/TPBow.java
+++ src/main/java/com/jokerhub/paper/plugin/orzmc/commands/TPBow.java
@@ -37,7 +37,7 @@ public class TPBow implements CommandExecutor {
 
             ItemStack teleport_bow = new ItemStack(Material.BOW);
             ItemMeta meta = teleport_bow.getItemMeta();
-            meta.addEnchant(Enchantment.ARROW_INFINITE,1,true);
+            meta.addEnchant(Enchantment.INFINITY,1,true);
             TextComponent name = Component.text(TPBow.name);
             meta.displayName(name);
             ArrayList<Component> loreList = new ArrayList<>();
