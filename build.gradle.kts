0c29af45b80ecb8eb1b8f2035b4275514290a4a3
wangzhizhou <824219521@qq.com>
2024-09-27 20:42:01 +0800

    add directly run minecraft server for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,27 @@
-plugins {
-    kotlin("jvm") version "2.0.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-}
-
 group = "com.jokerhub.paper.plugin"
 version = "1.0"
 description = "OrzMC"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    // 参考：https://docs.papermc.io/paper/dev/debugging#using-direct-debugging
+    // gradle-plugin: https://github.com/jpenilla/run-task#basic-usage
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.21.1")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
 
 // Paper Project Setup: https://docs.papermc.io/paper/dev/project-setup
 repositories {
@@ -21,13 +37,9 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     implementation("org.apache.httpcomponents:httpasyncclient:4.1.5")
     implementation("net.dv8tion:JDA:5.1.0") {
-        exclude(module="opus-java")
+        exclude(module = "opus-java")
     }
     implementation("org.java-websocket:Java-WebSocket:1.5.7")
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks.build {
